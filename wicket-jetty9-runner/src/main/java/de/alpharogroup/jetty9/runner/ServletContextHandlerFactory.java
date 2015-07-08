72a44e281b915418bb026591025625e61057c2ad
astrapi69 <asterios.raptis@gmx.net>
2015-07-08 16:44:21 +0200

    New factory for ServletContextHandler

---
+++ wicket-jetty9-runner/src/main/java/de/alpharogroup/jetty9/runner/ServletContextHandlerFactory.java
@@ -0,0 +1,177 @@
+package de.alpharogroup.jetty9.runner;
+
+import java.io.File;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.DispatcherType;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.wicket.Application;
+import org.apache.wicket.protocol.http.ContextParamWebApplicationFactory;
+import org.apache.wicket.protocol.http.WicketFilter;
+import org.apache.wicket.util.lang.Generics;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import de.alpharogroup.file.search.PathFinder;
+import de.alpharogroup.jetty9.runner.config.FilterHolderConfiguration;
+import de.alpharogroup.jetty9.runner.config.ServletContextHandlerConfiguration;
+import de.alpharogroup.jetty9.runner.config.ServletHolderConfiguration;
+
+public class ServletContextHandlerFactory
+{
+
+	public static ServletContextHandler newServletContextHandler(
+		ServletContextHandlerConfiguration configuration)
+	{
+		final ServletContextHandler context;
+		if (configuration.getParent() != null)
+		{
+			context = new ServletContextHandler(configuration.getParent(),
+				configuration.getContextPath());
+		}
+		else
+		{
+			context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		}
+		context.setContextPath(configuration.getContextPath());
+
+		context.setResourceBase(configuration.getWebapp().getAbsolutePath());
+
+		final FilterHolder filter = new FilterHolder(WicketFilter.class);
+		filter.setInitParameter(ContextParamWebApplicationFactory.APP_CLASS_PARAM, configuration
+			.getApplicationClass().getName());
+		for (Entry<String, String> initParameter : configuration.getInitParameters().entrySet())
+		{
+			filter.setInitParameter(initParameter.getKey(), initParameter.getValue());
+		}
+		context.addFilter(filter, configuration.getFilterPath(),
+			EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR));
+		context.addServlet(DefaultServlet.class, configuration.getFilterPath());
+
+		context.getSessionHandler().getSessionManager()
+			.setMaxInactiveInterval(configuration.getMaxInactiveInterval());
+		return context;
+	}
+
+	public static ServletContextHandler newServletContextHandler(
+		Class<? extends Application> applicationClass, String contextPath, File webapp,
+		int maxInactiveInterval, String filterPath)
+	{
+		Map<String, String> initParameters = Generics.newHashMap();
+		initParameters.put(WicketFilter.FILTER_MAPPING_PARAM, filterPath);
+		return newServletContextHandler(ServletContextHandlerConfiguration.builder()
+			.applicationClass(applicationClass).contextPath(contextPath).webapp(webapp)
+			.maxInactiveInterval(maxInactiveInterval)
+			.initParameter(WicketFilter.FILTER_MAPPING_PARAM, filterPath).filterPath(filterPath)
+			.build());
+	}
+	public static ServletContextHandler newServletContextHandler(
+		Class<? extends Application> applicationClass, File webapp)
+	{
+		return newServletContextHandler(applicationClass, "/", webapp, 300, "/*");
+	}
+
+	public static ServletContextHandler newServletContextHandler(
+		Class<? extends Application> applicationClass)
+	{
+		return newServletContextHandler(applicationClass, "/", PathFinder.getSrcMainJavaDir(), 300,
+			"/*");
+	}
+
+	public static ServletContextHandler getNewServletContextHandler(
+		ServletContextHandlerConfiguration configuration)
+	{
+		final ServletContextHandler context = new ServletContextHandler(
+			ServletContextHandler.SESSIONS);
+		context.setContextPath(configuration.getContextPath());
+
+		context.setResourceBase(configuration.getWebapp().getAbsolutePath());
+
+		context.getSessionHandler().getSessionManager()
+			.setMaxInactiveInterval(configuration.getMaxInactiveInterval());
+
+		initializeFilterHolder(configuration, context);
+
+		initializeServletHolder(configuration, context);
+
+		for (Entry<String, String> initParameter : configuration.getInitParameters().entrySet())
+		{
+			context.setInitParameter(initParameter.getKey(), initParameter.getValue());
+		}
+		return context;
+	}
+
+
+	private static void initializeFilterHolder(ServletContextHandlerConfiguration configuration,
+		final ServletContextHandler context)
+	{
+		List<FilterHolderConfiguration> filterHolderConfigurations = configuration
+			.getFilterHolderConfigurations();
+		if (CollectionUtils.isNotEmpty(filterHolderConfigurations))
+		{
+			for (FilterHolderConfiguration filterHolderConfiguration : filterHolderConfigurations)
+			{
+				final FilterHolder filter = new FilterHolder(
+					filterHolderConfiguration.getFilterClass());
+				if (StringUtils.isNotEmpty(filterHolderConfiguration.getName()))
+				{
+					filter.setName(filterHolderConfiguration.getName());
+				}
+				if (MapUtils.isNotEmpty(filterHolderConfiguration.getInitParameters()))
+				{
+					for (Entry<String, String> initParameter : filterHolderConfiguration
+						.getInitParameters().entrySet())
+					{
+						filter.setInitParameter(initParameter.getKey(), initParameter.getValue());
+					}
+				}
+				if (StringUtils.isNotEmpty(filterHolderConfiguration.getFilterPath()))
+				{
+					context.addFilter(filter, filterHolderConfiguration.getFilterPath(),
+						EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR));
+				}
+			}
+		}
+	}
+
+	private static void initializeServletHolder(ServletContextHandlerConfiguration configuration,
+		final ServletContextHandler context)
+	{
+		List<ServletHolderConfiguration> servletHolderConfigurations = configuration
+			.getServletHolderConfigurations();
+		if (CollectionUtils.isNotEmpty(servletHolderConfigurations))
+		{
+			for (ServletHolderConfiguration servletHolderConfiguration : servletHolderConfigurations)
+			{
+				ServletHolder servletHolder = new ServletHolder(
+					servletHolderConfiguration.getServletClass());
+				String servletName = servletHolderConfiguration.getName();
+				if (StringUtils.isNotEmpty(servletName))
+				{
+					servletHolder.setName(servletHolderConfiguration.getName());
+				}
+				if (MapUtils.isNotEmpty(servletHolderConfiguration.getInitParameters()))
+				{
+					for (Entry<String, String> initParameter : servletHolderConfiguration
+						.getInitParameters().entrySet())
+					{
+						servletHolder.setInitParameter(initParameter.getKey(),
+							initParameter.getValue());
+					}
+				}
+				if (StringUtils.isNotEmpty(servletHolderConfiguration.getPathSpec()))
+				{
+					context.addServlet(servletHolder, servletHolderConfiguration.getPathSpec());
+				}
+			}
+		}
+	}
+}
