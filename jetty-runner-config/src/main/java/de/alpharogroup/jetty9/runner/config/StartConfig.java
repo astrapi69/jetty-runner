233c989dd69abc09507b5e94f9db8a20c69a1c6c
jimmyMaci <tyler.durden69@gmx.de>
2016-09-30 15:38:13 +0200

    Create StartConfig.java

---
+++ jetty-runner-config/src/main/java/de/alpharogroup/jetty9/runner/config/StartConfig.java
@@ -0,0 +1,27 @@
+package de.alpharogroup.jetty9.runner.config;
+
+import java.io.File;
+
+public class StartConfig {
+
+	private String projectName;
+	
+	private String applicationName;
+	
+	private String runtimeConfigurationType;
+	
+	private int sessionTimeout;
+	
+	private File projectDirectory;
+	
+	private File webapp;
+	
+	private String filterPath;
+	
+	private String keyStorePassword;
+	
+	private String keyStorePathResource;
+	
+	private String contextPath;
+
+}
